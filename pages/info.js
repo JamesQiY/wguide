2848a08fb787a7506bca61b41514171b3cd43ccc
jams <jamesqiyang@gmail.com>
2021-12-08 20:42:26 -0500

    added search box in links

--- pages/info.js
+++ pages/info.js
@@ -1,21 +1,32 @@
-import React from 'react'
-import { PostWidget, CategoriesWidget } from '../components'
+import React, {useState} from 'react'
 import { getLinks } from '../services'
+import SearchBox from '../components/SearchBox'
 
 const info = ({ links }) => {
+  const [search, setsearch] = useState('')
+
+  const handleChange = (e) => {
+    setsearch(e.target.value)
+  }
+
+  const filteredLinks = links.filter(link => (
+    link.title.toLowerCase().includes(search.toLowerCase())
+  ))
+
   return (
     <div className="container mx-auto px-10 mb-8">
       <title>Wguides</title>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className='lg:col-span-12 col-span-1'>
           <div className='bg-white shadow-lg rounded-lg p-0 lg:p-8 mb-8'>
-            <div className='text-lg m-4 p-4 text-center'>Information and links</div>
-            <p className='mb-4 text-center'>This site is a collection of Wargroove related guides in a blog style.
+            <div className='text-xl m-4 p-4 text-center font-bold'>Information and links</div>
+            <p className='mb-4 p-1 text-center'>This site is a collection of Wargroove related guides in a blog style.
               Made by jams. If you have any questions or suggestions, you can find me in the official wargroove discord.
             </p>
-            <div className='relative overflow-hidden shadow-md rounded-lg p-4 pb-8'>
+            <SearchBox placeholder={'Link Search'} handleChange={handleChange}/>
+            <div className='relative overflow-hidden shadow-md rounded-lg m-1 p-4 pb-8'>
               Links
-              {links.map((link, index) =>
+              {filteredLinks.map((link, index) =>
                 <div key={index} className='p-2'>
                   <a href={link.address} target="_blank" rel="noopener noreferrer">
                     <div className='inline-block' >
