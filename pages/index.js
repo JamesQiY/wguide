16c2ca494e52fb316909ddf59e9941e04bf3a338
jams <jamesqiyang@gmail.com>
2022-01-14 15:46:30 -0500

    fixing styling typo

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home({ posts }) {
     <div className="container sm:mx-auto px-4 mb-8">
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-4'>
         {breakpoint ? <SearchBox placeholder='search' handleChange={handleChange} /> : <></> }
-        <div className='col-span-1 lg:col-span-9 xl:col-span-8'>
+        <div className='col-span-1 lg:col-span-8'>
           {filteredPosts.map((post, index) => <PostCard post={post.node} key={index} />)}
         </div>
         <SideWidgets> 
